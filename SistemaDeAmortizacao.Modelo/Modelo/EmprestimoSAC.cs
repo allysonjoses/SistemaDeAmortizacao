f6285ac63850854e4da6968019f4954ec880211b
Allyson José da Silva <allyson.jose@outlook.com>
2016-03-14 17:45:27 -0300

    Implementação do SAC

--- SistemaDeAmortizacao.Modelo/Modelo/EmprestimoSAC.cs
+++ SistemaDeAmortizacao.Modelo/Modelo/EmprestimoSAC.cs
@@ -18,7 +18,33 @@ namespace SistemaDeAmortizacao.Modelo.Modelo
         /// <returns>Lista de Parcelas do Emprestimo</returns>
         public override List<Parcela> GerarEmprestimo()
         {
-            throw new NotImplementedException();
+            List<Parcela> parcelas = new List<Parcela>();
+
+            double a = Valor/QtdParcelas; //Amortização
+            double s = Valor; //Saldo Devedor
+            double jurosMesal = Juros / 12;
+
+            Parcela parcelaZero = new Parcela(0, 0, 0, s, "0");
+            parcelas.Add(parcelaZero);
+
+            for (int i = 1; i < QtdParcelas; i++)
+            {
+                double j = s * jurosMesal / 100; //Juros
+                double p = a + j; //Prestação
+
+                Parcela x = new Parcela(p, j, a, s, i.ToString());
+                parcelas.Add(x);
+
+                s -= a;
+            }
+
+            double totalPrestacao = parcelas.Sum(p => p.Prestacao);
+            double totalJuros = totalPrestacao - Valor;
+
+            Parcela parcelaResultado = new Parcela(totalPrestacao, totalJuros, Valor, s, "TOTAL");
+            parcelas.Add(parcelaResultado);
+
+            return new List<Parcela>();
         }
     }
 }
