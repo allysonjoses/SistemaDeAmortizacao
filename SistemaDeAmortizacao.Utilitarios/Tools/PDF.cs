2bc0dc5ad1547322b849e1f82cbc82b7f299e49b
Allyson José da Silva <allyson.jose@outlook.com>
2016-03-15 12:05:43 -0300

    PDF

---
+++ SistemaDeAmortizacao.Utilitarios/Tools/PDF.cs
@@ -0,0 +1,69 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html;
+using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SistemaDeAmortizacao.Utilitarios.Tools
+{
+    public class PDF
+    {
+
+        private static void ExportToPdf(DataGrid grid, string name)
+        {
+            PdfPTable table = new PdfPTable(grid.Columns.Count);
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter writer = PdfWriter.GetInstance(doc, new System.IO.FileStream(name, System.IO.FileMode.Create));
+            doc.Open();
+            for (int j = 0; j < grid.Columns.Count; j++)
+            {
+                table.AddCell(new Phrase(grid.Columns[j].Header.ToString()));
+            }
+            table.HeaderRows = 1;
+            IEnumerable itemsSource = grid.ItemsSource as IEnumerable;
+            if (itemsSource != null)
+            {
+                foreach (var item in itemsSource)
+                {
+                    DataGridRow row = grid.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+                    if (row != null)
+                    {
+                        DataGridCellsPresenter presenter = FindVisualChild<DataGridCellsPresenter>(row);
+                        for (int i = 0; i < grid.Columns.Count; ++i)
+                        {
+                            DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(i);
+                            TextBlock txt = cell.Content as TextBlock;
+                            if (txt != null)
+                            {
+                                table.AddCell(new Phrase(txt.Text));
+                            }
+                        }
+                    }
+                }
+
+                doc.Add(table);
+                doc.Close();
+            }
+        }
+
+        private static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+    }
+}
